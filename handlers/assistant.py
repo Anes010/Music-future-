b7c7421412264fe51928078b95c6b1ef87d0e5c5
Anes010 <98156562+Anes010@users.noreply.github.com>
2022-05-28 15:34:16 +0300

    Update assistant.py

--- handlers/assistant.py
+++ handlers/assistant.py
@@ -8,7 +8,7 @@ from callsmusic.callsmusic import client as user
 
 
 @Client.on_message(
-    command(["join", "assistant", " userbotjoin"]) & ~filters.private & ~filters.bot
+    command(["join", "assistant", " userbotjoin", "انضم"]) & ~filters.private & ~filters.bot
 )
 @authorized_users_only
 async def join_chat(c: Client, m: Message):
@@ -23,7 +23,7 @@ async def join_chat(c: Client, m: Message):
             (await user.get_me()).id,
             can_manage_voice_chats=True
         )
-        return await user.send_message(chat_id, "🙂ᴀssɪsᴛᴀɴᴛ sᴜᴄᴄᴇssꜰᴜʟʟʏ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴄʜᴀᴛ ʙᴀʙʏ.​")
+        return await user.send_message(chat_id, "🙂تم الانضمام بنجاح الى الكروب.​")
     except UserAlreadyParticipant:
         admin = await m.chat.get_member((await user.get_me()).id)
         if not admin.can_manage_voice_chats:
@@ -31,5 +31,5 @@ async def join_chat(c: Client, m: Message):
                 (await user.get_me()).id,
                 can_manage_voice_chats=True
             )
-            return await user.send_message(chat_id, "🙂ᴀssɪsᴛᴀɴᴛ ᴀʟʀᴇᴀᴅʏ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴄʜᴀᴛ ʙᴀʙʏ.​")
-        return await user.send_message(chat_id, "🙂ᴀssɪsᴛᴀɴᴛ ᴀʟʀᴇᴀᴅʏ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴄʜᴀᴛ ʙᴀʙʏ.​")
+            return await user.send_message(chat_id, "🙂حساب المساعد بالفعل هوه داخل الكروب.​")
+        return await user.send_message(chat_id, "🙂حساب المساعد بالفعل هوه داخل الكروب.​")
